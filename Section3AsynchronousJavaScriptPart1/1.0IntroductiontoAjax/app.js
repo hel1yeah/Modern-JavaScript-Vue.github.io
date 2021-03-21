2fa661dc2583300d886884b77e83ea56818beca2
hel1yeah <hel1_yeah@ukr.net>
2021-03-22 00:54:09 +0200

    get users, use ajax request

--- Section3AsynchronousJavaScriptPart1/1.0IntroductiontoAjax/app.js
+++ Section3AsynchronousJavaScriptPart1/1.0IntroductiontoAjax/app.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector("button");
-const container = document.querySelector('.container')
+const container = document.querySelector(".container");
 
 function getPosts(cb) {
   const xhr = new XMLHttpRequest();
@@ -10,7 +10,7 @@ function getPosts(cb) {
 
   function accessLoading() {
     const response = JSON.parse(xhr.responseText);
-    // console.log(response);
+    console.log(response);
     cb(response);
   }
 
@@ -21,28 +21,27 @@ function getPosts(cb) {
   xhr.send();
 }
 
-
 function renderPosts(response) {
   const fragment = document.createDocumentFragment();
-    response.forEach((post) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-      const title = document.createElement("h5");
-      title.classList.add("card-title");
-      title.textContent = post.title
-      const article = document.createElement("p");
-      title.classList.add("card-text");
-      article.textContent = post.body
-      cardBody.append(title);
-      cardBody.append(article);
-      card.append(cardBody)
-      fragment.append(card)
-    });
-    container.append(fragment)
+  response.forEach((post) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    const title = document.createElement("h5");
+    title.classList.add("card-title");
+    title.textContent = post.title;
+    const article = document.createElement("p");
+    title.classList.add("card-text");
+    article.textContent = post.body;
+    cardBody.append(title);
+    cardBody.append(article);
+    card.append(cardBody);
+    fragment.append(card);
+  });
+  container.append(fragment);
 }
 
-btn.addEventListener("click", (e) => {
+btn.addEventListener("click", e => {
   getPosts(renderPosts);
 });
--- Section3AsynchronousJavaScriptPart1/1.1homeWorkIntroductiontoAjax/app.js
+++ Section3AsynchronousJavaScriptPart1/1.1homeWorkIntroductiontoAjax/app.js
@@ -1,15 +1,51 @@
-
-
-function getPosts(cb) {
+const button = document.querySelector("button");
+const container = document.querySelector(".container");
+function getUsers(cb) {
   const xhr = new XMLHttpRequest();
-
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+  xhr.open("get", "https://jsonplaceholder.typicode.com/users");
 
   xhr.addEventListener("load", accessLoading);
+  xhr.addEventListener("error ", errorLoading);
 
   function accessLoading() {
     const response = JSON.parse(xhr.responseText);
-    // console.log(response);
+    console.log(xhr.status);
     cb(response);
   }
+
+  function errorLoading() {
+    console.log(error);
+  }
+
+  xhr.send();
 }
+
+function renderUserFragment(response) {
+  const fragment = document.createDocumentFragment();
+  response.forEach((user) => {
+    console.log(user);
+
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+
+    const h4 = document.createElement("h5");
+    h4.classList.add("header-name");
+    h4.textContent = `${user.name}, ${user.username}`
+
+    const descr = document.createElement('p')
+    descr.classList.add('descr')
+    descr.innerHTML = `${user.company.bs}, <br> ${user.company.catchPhrase}`
+
+    wrapper.append(h4)
+    wrapper.append(descr)
+
+    fragment.append(wrapper)
+
+    container.append(fragment)
+
+  });
+}
+
+button.addEventListener("click", () => {
+  getUsers(renderUserFragment);
+});
